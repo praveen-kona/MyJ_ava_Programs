0636fb8d59da33980a94d0903aae412870c25408
praveen-kona <ntrjr8989gmail.com>
2026-06-30 18:03:13 +0530

    add thes extra files

---
+++ myfirstjavaproject/src/com/javaintroduction/Sumofarray.java
@@ -0,0 +1,28 @@
+package com.javaintroduction;
+import java.util.Scanner;
+
+public class Sumofarray 
+{
+	public int arraysum()
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[5];
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+	      	sum+=arr[i];
+	    }
+		return sum;
+	}
+
+	public static void main(String[] args) 
+	{	
+		Sumofarray s1=new Sumofarray();
+		int res=s1.arraysum();
+		System.out.println(res);
+		
+	}
+
+}
