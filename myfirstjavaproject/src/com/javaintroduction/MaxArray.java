3d424c780c7f0bdbdebf58ae80df0ac47a18542a
praveen-kona <ntrjr8989gmail.com>
2026-07-13 09:03:10 +0530

    commit

---
+++ myfirstjavaproject/src/com/javaintroduction/MaxArray.java
@@ -0,0 +1,23 @@
+package com.javaintroduction;
+
+public class MaxArray
+{
+    void mein()
+    {
+        int[] arr={1,2,5,6,7};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    public static void main(String[] args)
+    {
+        MaxArray a1=new MaxArray();
+        a1.mein();
+    }
+}
---
+++ myfirstjavaproject/src/com/javaintroduction/PalindromeOfString.java
@@ -0,0 +1,32 @@
+package com.javaintroduction;
+
+public class PalindromeOfString {
+
+	String s1="121";
+	String s2=s1.toLowerCase();
+	StringBuilder sb=new StringBuilder();
+	void method()
+	{
+		for(int i=s2.length()-1;i>=0;i--)
+		{
+			sb.append(s2.charAt(i));
+		}
+		String s3=sb.toString();
+		if(s2.equals(s3))
+		{
+			System.out.println("palindrome");
+		}
+		else
+		{
+			System.out.println("not");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		PalindromeOfString p1=new PalindromeOfString();
+		p1.method();
+	}
+
+}
