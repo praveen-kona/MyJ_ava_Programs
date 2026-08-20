a7a2365c522bb7aa101d49f6450a894e5634b02a
praveen-kona <ntrjr8989@gmail.com>
2026-08-20 19:04:08 +0530

    updated

--- myfirstjavaproject/src/com/jvm/Jvm.java
+++ myfirstjavaproject/src/com/jvm/Jvm.java
@@ -5,7 +5,7 @@ public class Jvm {
 	public static void main(String[] args) throws  ClassNotFoundException{
 		var arr=new int[5];
 		System.out.println(((Object)arr).getClass().getSimpleName());
-		Class.forName("hello");
+		
 
 	}
 
