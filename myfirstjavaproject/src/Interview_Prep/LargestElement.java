c03137f79f75917891fa6624881719a93e7eec3d
praveen-kona <ntrjr8989@gmail.com>
2026-08-26 10:17:17 +0530

    day 1

---
+++ myfirstjavaproject/src/Interview_Prep/LargestElement.java
@@ -0,0 +1,20 @@
+package Interview_Prep;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {3,4,2};
+		int max=arr[0];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>max)
+			{
+				max=arr[i];
+			}
+		}
+		System.out.println(max);
+
+	}
+
+}
