a7a7451b3284eeaded0a48f60fcf8fa56d76641d
praveen-kona <ntrjr8989@gmail.com>
2026-09-02 19:43:57 +0530

    abstract classes

---
+++ myfirstjavaproject/src/Abstract_Classes/Abstract_1.java
@@ -0,0 +1,11 @@
+package Abstract_Classes;
+
+public abstract class Abstract_1 {
+	abstract void start();
+	 
+	void stop()
+	{
+		System.out.println("Vehicle stopped");
+	}
+
+}
---
+++ myfirstjavaproject/src/Abstract_Classes/Class_ab_1.java
@@ -0,0 +1,17 @@
+package Abstract_Classes;
+
+public class Class_ab_1 extends Abstract_1{
+	public void start()
+	{
+		System.out.println("vehicle started");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class_ab_1 c=new Class_ab_1();
+		c.start();
+		c.stop();
+
+	}
+
+}
