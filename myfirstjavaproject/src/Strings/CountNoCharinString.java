31f244524caac3317c9139347bcebb83f1e8a796
praveen-kona <ntrjr8989@gmail.com>
2026-09-20 11:42:51 +0530

    strings

---
+++ myfirstjavaproject/src/Strings/CountNoCharinString.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class CountNoCharinString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello";
+		System.out.println(s.trim().length());
+
+	}
+
+}
