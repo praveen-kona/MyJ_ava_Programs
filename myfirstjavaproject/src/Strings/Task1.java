7808cbc8cde5552bb4a8c93c9964945aa2676162
praveen-kona <ntrjr8989@gmail.com>
2026-09-20 09:18:01 +0530

    strings

---
+++ myfirstjavaproject/src/Strings/Task1.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hello";
+		s.concat("hi");
+		System.out.println(s);
+	}
+
+}
