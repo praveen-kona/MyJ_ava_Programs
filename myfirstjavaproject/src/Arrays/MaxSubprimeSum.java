e68850105aa2f198b9d2696dff798db84cf6b25c
praveen-kona <ntrjr8989@gmail.com>
2026-09-21 17:51:34 +0530

    strings

--- myfirstjavaproject/src/Arrays/MaxSubprimeSum.java
+++ myfirstjavaproject/src/Arrays/MaxSubprimeSum.java
@@ -1,38 +1,39 @@
 package Arrays;
 
-public class MaxSubArraySum {
+public class MaxSubprimeSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr= {1, 2, 3, 2, 1};
+		int[] arr={1,2,3,4,5};
 		int max=Integer.MIN_VALUE;
-		int count=0;
-		int target=5;
-		for(int i=0;i<arr.length;i++) {
+		for(int i=0;i<arr.length;i++)
+		{
 			for(int j=i;j<arr.length;j++)
 			{
 				int sum=0;
-				
 				for(int k=i;k<=j;k++)
 				{
 					sum+=arr[k];
-					
 				}
-				System.out.println(sum);
-				if(sum==target)
-				{
-					count++;
-				}
-
 				if(sum>max)
-				{
-					max=sum;
+				{int count=0;
+					for(int l=1;l<=sum;l++)
+					{
+						
+						if(sum%l==0)
+						{
+							count++;
+						}
+					}
+					if(count==2)
+					{
+						max=sum;
+					}
 				}
+				
 			}
-			
 		}
 		System.out.println(max);
-		System.out.println(count+"count");
 
 	}
 
