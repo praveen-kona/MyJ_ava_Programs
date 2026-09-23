fac12354a9c46d8a9d977cedea72a3b32eb584b0
praveen-kona <ntrjr8989@gmail.com>
2026-09-23 10:13:59 +0530

    Strings

--- myfirstjavaproject/src/ArraysandStrings/RemoveSpace.java
+++ myfirstjavaproject/src/ArraysandStrings/RemoveSpace.java
@@ -7,10 +7,17 @@ public class RemoveSpace {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s="hello java how are u";
-		String[] str=s.split(" ");
-		for(int i=0;i<str.length;i++)
+		s=s.replace(" ","");
+		System.out.println(s);
+		
+		//
+		String s2="hello jav ahi ";
+		for(int i=0;i<s2.length();i++ )
 		{
-			System.out.print(str[i]);
+			if(s2.charAt(i)!=' ')
+			{
+				System.out.print(s2.charAt(i));
+			}
 		}
 		
 	}
